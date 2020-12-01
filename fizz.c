ebfae557b33afae4250f55325bb0e8f100fd81f8
NaouaiSafa <40889008+NaouaiSafa@users.noreply.github.com>
2020-12-01 16:25:32 +0100

    Update fizz.c

--- fizz.c
+++ fizz.c
@@ -8,7 +8,7 @@ for(i=1; i<=100; i++)
 
     else if (((i%3)==0)&&((i%7)!=0))
     {printf("Fizz\n");}
-    else if (((i%5)==0)&&((i%7)!=0))
+    else if (((i%3)==0)&&((i%7)!=0))
     {printf("Buzz\n");}
 
     else
