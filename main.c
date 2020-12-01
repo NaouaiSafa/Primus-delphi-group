2c4d6c22533315279a345d4db1b4cb2393061f02
NaouaiSafa <40889008+NaouaiSafa@users.noreply.github.com>
2020-12-01 15:00:49 +0100

    Add files via upload
    the same main.c but prints fussi instead of fuzzi if the number is multiple of 3

--- main.c
+++ main.c
@@ -4,10 +4,10 @@ int main()
 {int i;
 for(i=1; i<=100; i++)
     {if (((i%3)==0)&&((i%5)==0))
-    {printf("FizzBuzz\n");}
+    {printf("FissBuzz\n");}
 
     else if (((i%3)==0)&&((i%5)!=0))
-    {printf("Fizz\n");}
+    {printf("Fiss\n");}
     else if (((i%5)==0)&&((i%3)!=0))
     {printf("Buzz\n");}
 
