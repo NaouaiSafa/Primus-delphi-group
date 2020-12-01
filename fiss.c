4890fa20623bcfbe0a414c59c4fe7e70e811a28c
NaouaiSafa <40889008+NaouaiSafa@users.noreply.github.com>
2020-12-01 16:22:26 +0100

    Update fiss.c
    print fiss using the numbers 3 and 7 for departmentB

--- fiss.c
+++ fiss.c
@@ -3,12 +3,12 @@
 int main()
 {int i;
 for(i=1; i<=100; i++)
-    {if (((i%3)==0)&&((i%5)==0))
+    {if (((i%3)==0)&&((i%7)==0))
     {printf("FissBuzz\n");} //print fissbuzz instead of fizz
 
-    else if (((i%3)==0)&&((i%5)!=0))
+    else if (((i%3)==0)&&((i%7)!=0))
     {printf("Fiss\n");} // print fiss instead of fizz
-    else if (((i%5)==0)&&((i%3)!=0))
+    else if (((i%7)==0)&&((i%3)!=0))
     {printf("Buzz\n");}
 
     else
