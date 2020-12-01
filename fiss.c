f18aaaa80c80586bc9cb87f30282152bc0e4fb57
NaouaiSafa <40889008+NaouaiSafa@users.noreply.github.com>
2020-12-01 15:32:59 +0100

    Update fiss.c

--- fiss.c
+++ fiss.c
@@ -4,10 +4,10 @@ int main()
 {int i;
 for(i=1; i<=100; i++)
     {if (((i%3)==0)&&((i%5)==0))
-    {printf("FissBuzz\n");} //print fiss instead of fizz
+    {printf("FissBuzz\n");} //print fissbuzz instead of fizz
 
     else if (((i%3)==0)&&((i%5)!=0))
-    {printf("Fiss\n");}
+    {printf("Fiss\n");} // print fiss instead of fizz
     else if (((i%5)==0)&&((i%3)!=0))
     {printf("Buzz\n");}
 
